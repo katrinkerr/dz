739ec13f643085e56321586ed34da6544459417d
katrinkerr <katya2009.savchenko@gmail.com>
2025-01-30 16:19:54 +0200

    Initial commit

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введіть номер для тижня: ");
+int dayNumber = int.Parse(Console.ReadLine());
+
+switch (dayNumber)
+{
+    case 1:
+        Console.WriteLine("Понеділок");
+        break;
+    case 2:
+        Console.WriteLine("Вівторок");
+        break;
+    case 3:
+        Console.WriteLine("Середа");
+        break;
+    case 4:
+        Console.WriteLine("Четвер");
+        break;
+    case 5:
+        Console.WriteLine("П'ятниця");
+        break;
+    case 6:
+        Console.WriteLine("Субота");
+        break;
+    case 7:
+        Console.WriteLine("Неділя");
+        break;
+    default:
+        Console.WriteLine("Невірний номер дня. Введіть число від 1 до 7.");
+        break;
+}
