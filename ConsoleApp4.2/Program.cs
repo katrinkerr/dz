0200f99053f03636219eaf07cedbaf7e84e55a68
katrinkerr <katya2009.savchenko@gmail.com>
2025-01-30 22:45:13 +0200

    Initial commit

---
+++ ConsoleApp4.2/Program.cs
@@ -0,0 +1,44 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Введіть перше число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введіть друге число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введіть операцію (+, -, *, /): ");
+int operation =Convert.ToInt32(Console.ReadLine());
+int result;
+switch (operation)
+{
+    case 1:
+        result = num1 + num2;
+        Console.WriteLine($"Результат: {result}");
+        break;
+
+    case 2:
+        result = num1 - num2;
+        Console.WriteLine($"Результат: {result}");
+        break;
+
+    case 3:
+        result = num1 * num2;
+        Console.WriteLine($"Результат: {result}");
+        break;
+
+    case 4:
+        if (num2 != 0)
+        {
+            double dresult = (double)num1 / num2;
+            Console.WriteLine($"Результат:{dresult}");
+        }
+        else
+        {
+            Console.WriteLine("Помилка: ділення на нуль!");
+        }
+        break;
+    default:
+        Console.WriteLine("Невірна операція");
+        break;
+}
+
+
+    
+
