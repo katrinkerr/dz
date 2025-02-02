42da0ecba2755a9ccd656b01183346bfd6bca326
katrinkerr <katya2009.savchenko@gmail.com>
2025-02-02 21:16:33 +0200

    Initial commit

---
+++ ConsoleApp5.2/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Введіть число: ");
+string a = Console.ReadLine();
+try
+{
+    double number = Convert.ToDouble(a);
+    Console.WriteLine($"Ви ввели число: {number}");
+}
+catch (FormatException)
+{
+    Console.WriteLine("Помилка: введене значення не є числом.");
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Сталася помилка: {ex.Message}");
+}
+
+
