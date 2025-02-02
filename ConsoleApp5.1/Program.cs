6023c96d585ce2f4285d0b9a6bd55cd12871493b
katrinkerr <katya2009.savchenko@gmail.com>
2025-02-02 20:47:34 +0200

    Initial commit

---
+++ ConsoleApp5.1/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+double a, b;
+Console.WriteLine("Введіть перше число: ");
+a = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введіть друге число: ");
+b= Convert.ToDouble(Console.ReadLine());
+try
+{
+    Console.WriteLine(a / b);
+}
+catch(DivideByZeroException) 
+{
+    
+}
+Console.WriteLine("Ділення на 0 заборонено");
