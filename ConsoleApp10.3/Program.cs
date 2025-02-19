575ffa73c14c364ee692bc883dcae5196a4e3c1b
katrinkaia <katya2009.savchenko@gmail.com>
2025-02-19 21:44:31 +0200

    dz

---
+++ ConsoleApp10.3/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Text;
+class Program
+{
+    static void Main()
+    {
+        StringBuilder myStringBuilder = new StringBuilder();
+        Console.WriteLine("Введіть рядок ");
+        string substring =Console.ReadLine();
+        string result = substring.Replace(" ", "");
+        Console.WriteLine(result);
+    }
+}
+
+
+
