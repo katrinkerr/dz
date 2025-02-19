e9e94b7e4f817fd5c6e9b7de4da359eb47f02d7b
katrinkaia <katya2009.savchenko@gmail.com>
2025-02-19 22:00:22 +0200

    dz

---
+++ ConsoleApp10.1/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введіть ім'я та прізвище через пробіл:");
+string substring = Console.ReadLine();
+string[] p = substring.Split(' ');
+string name = p[0];
+string surname = p[1];
+if ((name[0]) == (surname[0]))
+{
+    Console.WriteLine("Прізвище починається на ту ж літеру, як ім’я");
+}
+else
+{
+    Console.WriteLine("Прізвище не починається на ту ж літеру, як ім’я");
+}
