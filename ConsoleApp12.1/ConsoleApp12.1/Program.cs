0c38c543f7218b359363f67c4dbb4c9be9f26962
katrinkaia <katya2009.savchenko@gmail.com>
2025-03-19 17:43:55 +0200

    dz

---
+++ ConsoleApp12.1/ConsoleApp12.1/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введіть номер числа Фібоначчі: ");
+        int n = int.Parse(Console.ReadLine());
+        Console.WriteLine($"Число Фібоначчі {n}-го порядку: {Fibonacci.Calculate(n)}");
+    }
+}
+
+static class Fibonacci
+{
+    public static int Calculate(int n)
+    {
+        if (n == 1) return 0;
+        if (n == 2) return 1;
+        return Calculate(n - 1) + Calculate(n - 2);
+    }
+}
